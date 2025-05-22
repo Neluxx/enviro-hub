3241821cdab07790219afabee123f4cb4ea257c9
Neluxx <fabian.arndt96@proton.me>
2025-05-22 15:10:49 +0200

    Add an example chart for testing purposes

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,16 +5,39 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\UX\Chartjs\Builder\ChartBuilderInterface;
+use Symfony\UX\Chartjs\Model\Chart;
 
 class HomeController extends AbstractController
 {
     #[Route('/')]
-    public function number(): Response
+    public function index(ChartBuilderInterface $chartBuilder): Response
     {
-        $number = random_int(0, 100);
+        $chart = $chartBuilder->createChart(Chart::TYPE_LINE);
 
-        return $this->render('home/home.html.twig', [
-            'number' => $number,
+        $chart->setData([
+            'labels' => ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+            'datasets' => [
+                [
+                    'label' => 'My First dataset',
+                    'backgroundColor' => 'rgb(255, 99, 132)',
+                    'borderColor' => 'rgb(255, 99, 132)',
+                    'data' => [0, 10, 5, 2, 20, 30, 45],
+                ],
+            ],
+        ]);
+
+        $chart->setOptions([
+            'scales' => [
+                'y' => [
+                    'suggestedMin' => 0,
+                    'suggestedMax' => 100,
+                ],
+            ],
+        ]);
+
+        return $this->render('home/index.html.twig', [
+            'chart' => $chart,
         ]);
     }
 }
