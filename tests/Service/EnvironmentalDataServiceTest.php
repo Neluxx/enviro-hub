46427b9bb64b88a2f57689488c8f0fd6ab09f021
Neluxx <fabian.arndt96@proton.me>
2025-12-27 12:13:55 +0100

    style: reorder imports in EnvironmentalDataServiceTest
    Refs: #17

--- tests/Service/EnvironmentalDataServiceTest.php
+++ tests/Service/EnvironmentalDataServiceTest.php
@@ -5,8 +5,8 @@ declare(strict_types=1);
 namespace App\Tests\Service;
 
 use App\Repository\EnvironmentalDataRepository;
-use App\Service\NotificationService;
 use App\Service\EnvironmentalDataService;
+use App\Service\NotificationService;
 use DateTime;
 use Exception;
 use InvalidArgumentException;
